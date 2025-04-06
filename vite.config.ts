0fc2bb60c754a18c1f79c083d6aa75bd485ab7a7
nawaraj bk <111752108+tesla39@users.noreply.github.com>
2025-04-06 21:51:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+     allowedHosts: [
+      "c3ecaa08-a12e-4a9a-9a4c-522908732a40.lovableproject.com"
+    ]
   },
   plugins: [
     react(),
